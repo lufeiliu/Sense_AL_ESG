95599bfe0446dc5fc02c48ff93fb780768020828
Dugast <adugast037@headmind.com>
2024-10-28 11:55:45 +0100

    first commit, testing

---
+++ test.py
@@ -0,0 +1,8 @@
+from gnews import GNews
+import newspaper
+
+google_news = GNews()
+paki = google_news.get_news('Pakistan')
+print(paki[0])
+article = google_news.get_full_article(paki[0]['url'])
+print(article)
