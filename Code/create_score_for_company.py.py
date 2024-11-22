00104eb2f65e1409edaf1defe4eddeaf2dfd5e1c
Adrien-Dugast <adrien.dugast@ensta-paris.fr>
2024-11-22 10:54:54 +0100

    creating score for monthly update

--- Code/create_score_for_company.py.py
+++ Code/create_score_for_company.py.py
@@ -1,4 +1,9 @@
 from download_article_from_search import extract_articles_from_parser, find_list_articles
+import os
+import sys
+sys.path.append(os.path.abspath('../database_management'))
+from add_a_row import add_row
+from datetime import date
 
 def create_score_for_company(company):
   query = company + " ESG"
@@ -7,15 +12,15 @@ def create_score_for_company(company):
 
   scores = [article["ESGscore"] for article in L]
   if len(L)!=0:
-    return(sum(L)/len(L))
+    return(sum(L)/len(L),nb_articles)
   else:
-    return(False)
+    return(False,False)
 
 def add_score_to_database(company):
-  score = create_score_for_company(company)
+  score, nb_articles = create_score_for_company(company)
 
   if score == False:
     print("error no article found")
     return(-1)
   else:
-    add_row()
+    add_row("database_ratings.db",(company,str(date.today()), score,nb_articles))
